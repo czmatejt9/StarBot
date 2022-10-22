c6d6b68e17ab2902e8e1061f97c007239dab2aac
czmatejt9 <105561491+czmatejt9@users.noreply.github.com>
2022-10-22 20:13:54 +0200

    changed log msg

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ handler = logging.handlers.RotatingFileHandler(
     backupCount=5,  # Rotate through 5 files
 )
 dt_fmt = '%Y-%m-%d %H:%M:%S'
-formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name:<15}: {message}', dt_fmt, style='{')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
