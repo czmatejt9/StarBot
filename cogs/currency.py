1ec9333eb2f39af101d072f937191f90f4433562
czmatejt9 <105561491+czmatejt9@users.noreply.github.com>
2022-10-25 07:36:43 +0200

    refactoring

--- cogs/currency.py
+++ cogs/currency.py
@@ -146,51 +146,12 @@ class Currency(commands.Cog):
         embed.set_author(name=member.display_name, icon_url=member.display_avatar)
         await ctx.reply(embed=embed)
 
-    @commands.hybrid_command(name="beg")
-    @app_commands.guilds(discord.Object(id=MY_GUILD_ID))
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    async def beg(self, ctx: commands.Context):
-        """Beg for money"""
-        wallet, bank = await self.get_balance(ctx.author.id)
-        if wallet + bank >= 1000:
-            msg = "You already have enough money!"
-        else:
-            money = random.randint(10, 100)
-            await self.transfer_money(CENTRAL_BANK_ID, ctx.author.id, money, 0, "begging")
-            if money < 20:
-                msg = f"Someone gave you {money}{CURRENCY_EMOTE}!"
-            elif money < 60:
-                msg = f"Someone tripped and {money}{CURRENCY_EMOTE} fell out of their pocket into your hand!"
-            else:
-                msg = f"{money}{CURRENCY_EMOTE} fell out of the sky and landed in your pocket!"
-        await ctx.reply(msg)
-
-    @commands.hybrid_command(name="crime")
-    @app_commands.guilds(discord.Object(id=MY_GUILD_ID))
-    @commands.cooldown(1, 60, commands.BucketType.user)
-    async def crime(self, ctx: commands.Context):
-        """Commit a crime"""
-        if random.randint(1, 100) <= 20:
-            money = random.randint(10, 100)
-            await self.transfer_money(ctx.author.id, CENTRAL_BANK_ID, money, 0, "failed crime")
-            msg = f"You got caught and had to pay {money}{CURRENCY_EMOTE} to the police!"
-        else:
-            money = random.randint(50, 500)
-            await self.transfer_money(CENTRAL_BANK_ID, ctx.author.id, money, 0, "successful crime")
-            # custom messages based on how much money you get
-            if money < 150:
-                msg = f"You stole {money}{CURRENCY_EMOTE}!"
-            elif money < 350:
-                msg = f"You stole {money}{CURRENCY_EMOTE} from a rich person!"
-            else:
-                msg = f"You stole {money}{CURRENCY_EMOTE} from a bank!"
-        await ctx.reply(msg)
-
     @commands.hybrid_command(name="deposit", aliases=["dep"])
     @app_commands.guilds(discord.Object(id=MY_GUILD_ID))
     @app_commands.describe(amount="normal number or 'all'")
+    @commands.cooldown(2, 86400, commands.BucketType.user)
     async def deposit(self, ctx: commands.Context, amount):
-        """Deposit money into your bank"""
+        """Deposit money into your bank (2 uses per day)"""
         wallet, bank = await self.get_balance(ctx.author.id)
         if amount == "all":
             amount = wallet
@@ -209,8 +170,9 @@ class Currency(commands.Cog):
     @commands.hybrid_command(name="withdraw", aliases=["with"])
     @app_commands.guilds(discord.Object(id=MY_GUILD_ID))
     @app_commands.describe(amount="normal number or 'all'")
+    @commands.cooldown(2, 86400, commands.BucketType.user)
     async def withdraw(self, ctx: commands.Context, amount):
-        """Withdraw money from your bank"""
+        """Withdraw money from your bank (2 uses per day)"""
         wallet, bank = await self.get_balance(ctx.author.id)
         if amount == "all":
             amount = wallet
@@ -244,44 +206,6 @@ class Currency(commands.Cog):
         taxed_amount = await self.transfer_money(ctx.author.id, member.id, amount, TAX, "user to user transfer")
         await ctx.reply(
             f"You sent {taxed_amount}{CURRENCY_EMOTE} to {member.mention}! ({amount - taxed_amount}{CURRENCY_EMOTE} lost to tax)")
-
-    @commands.hybrid_command(name="gamble")
-    @app_commands.guilds(discord.Object(id=MY_GUILD_ID))
-    @app_commands.describe(guess="number from 1 to 6", amount="normal number or 'all'")
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    async def gamble(self, ctx: commands.Context, guess: int, amount):
-        """Gamble your money against StarBot! Guess which number will be rolled on a 6-sided dice,
-         StarBot also takes a guess, if yours is closer you win"""
-        wallet, bank = await self.get_balance(ctx.author.id)
-        if amount == "all":
-            amount = wallet
-        elif amount is str:
-            return await ctx.reply("Amount must be a number or 'all'!")
-        if amount <= 0:
-            return await ctx.reply("Amount must be positive!")
-        if guess < 1 or guess > 6:
-            return await ctx.reply("Guess must be between 1 and 6!")
-        if amount > wallet:
-            return await ctx.reply("You don't have that much money!")
-
-        dice = random.randint(1, 6)
-        starbot_guess = random.randint(1, 6)
-        if abs(guess - dice) < abs(starbot_guess - dice):
-            await self.transfer_money(self.bot.id, ctx.author.id, amount, 0, "gambling")
-            embed = discord.Embed(title="You won!", color=discord.Color.green(),
-                                  description=f"You guessed {guess}. StarBot guessed {starbot_guess}.\n"
-                                              f"The dice rolled {dice}")
-        elif abs(guess - dice) > abs(starbot_guess - dice):
-            await self.transfer_money(ctx.author.id, self.bot.id, amount, 0, "gambling")
-            embed = discord.Embed(title="You lost!", color=discord.Color.red(),
-                                  description=f"You guessed {guess}. StarBot guessed {starbot_guess}.\n"
-                                              f"The dice rolled {dice}")
-        else:
-            embed = discord.Embed(title="It's a tie!", color=discord.Color.gold(),
-                                  description=f"You guessed {guess}. StarBot guessed {starbot_guess}.\n"
-                                              f"The dice rolled {dice}")
-        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar)
-        await ctx.reply(embed=embed)
 
     @commands.hybrid_command(name="leaderboard", aliases=["lb"])
     @app_commands.guilds(discord.Object(id=MY_GUILD_ID))
@@ -329,6 +253,91 @@ class Currency(commands.Cog):
                                               f"\nYour daily streak is now {daily_streak + 1}!")
             embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar)
             await ctx.reply(embed=embed)
+
+    @commands.hybrid_command(name="beg")
+    @app_commands.guilds(discord.Object(id=MY_GUILD_ID))
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def beg(self, ctx: commands.Context):
+        """Beg for money"""
+        wallet, bank = await self.get_balance(ctx.author.id)
+        if wallet + bank >= 1000:
+            msg = "You already have enough money!"
+        else:
+            money = random.randint(10, 100)
+            await self.transfer_money(CENTRAL_BANK_ID, ctx.author.id, money, 0, "begging")
+            if money < 20:
+                msg = f"Someone gave you {money}{CURRENCY_EMOTE}!"
+            elif money < 60:
+                msg = f"Someone tripped and {money}{CURRENCY_EMOTE} fell out of their pocket into your hand!"
+            else:
+                msg = f"{money}{CURRENCY_EMOTE} fell out of the sky and landed in your pocket!"
+        await ctx.reply(msg)
+
+    @commands.hybrid_command(name="crime")
+    @app_commands.guilds(discord.Object(id=MY_GUILD_ID))
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    async def crime(self, ctx: commands.Context):
+        """Commit a crime"""
+        if random.randint(1, 100) <= 20:
+            money = random.randint(10, 100)
+            await self.transfer_money(ctx.author.id, CENTRAL_BANK_ID, money, 0, "failed crime")
+            msg = f"You got caught and had to pay {money}{CURRENCY_EMOTE} to the police!"
+        else:
+            money = random.randint(50, 500)
+            await self.transfer_money(CENTRAL_BANK_ID, ctx.author.id, money, 0, "successful crime")
+            # custom messages based on how much money you get
+            if money < 150:
+                msg = f"You stole {money}{CURRENCY_EMOTE}!"
+            elif money < 350:
+                msg = f"You stole {money}{CURRENCY_EMOTE} from a rich person!"
+            else:
+                msg = f"You stole {money}{CURRENCY_EMOTE} from a bank!"
+        await ctx.reply(msg)
+
+    @commands.hybrid_command(name="rob")
+    @app_commands.guilds(discord.Object(id=MY_GUILD_ID))
+    @app_commands.describe(member="user to rob")
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    async def rob(self, ctx: commands.Context, member: discord.Member):
+        pass
+
+    @commands.hybrid_command(name="gamble")
+    @app_commands.guilds(discord.Object(id=MY_GUILD_ID))
+    @app_commands.describe(guess="number from 1 to 6", amount="normal number or 'all'")
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def gamble(self, ctx: commands.Context, guess: int, amount):
+        """Gamble your money against StarBot! Guess which number will be rolled on a 6-sided dice,
+         StarBot also takes a guess, if yours is closer you win"""
+        wallet, bank = await self.get_balance(ctx.author.id)
+        if amount == "all":
+            amount = wallet
+        elif amount is str:
+            return await ctx.reply("Amount must be a number or 'all'!")
+        if amount <= 0:
+            return await ctx.reply("Amount must be positive!")
+        if guess < 1 or guess > 6:
+            return await ctx.reply("Guess must be between 1 and 6!")
+        if amount > wallet:
+            return await ctx.reply("You don't have that much money!")
+
+        dice = random.randint(1, 6)
+        starbot_guess = random.randint(1, 6)
+        if abs(guess - dice) < abs(starbot_guess - dice):
+            await self.transfer_money(self.bot.id, ctx.author.id, amount, 0, "gambling")
+            embed = discord.Embed(title="You won!", color=discord.Color.green(),
+                                  description=f"You guessed {guess}. StarBot guessed {starbot_guess}.\n"
+                                              f"The dice rolled {dice}")
+        elif abs(guess - dice) > abs(starbot_guess - dice):
+            await self.transfer_money(ctx.author.id, self.bot.id, amount, 0, "gambling")
+            embed = discord.Embed(title="You lost!", color=discord.Color.red(),
+                                  description=f"You guessed {guess}. StarBot guessed {starbot_guess}.\n"
+                                              f"The dice rolled {dice}")
+        else:
+            embed = discord.Embed(title="It's a tie!", color=discord.Color.gold(),
+                                  description=f"You guessed {guess}. StarBot guessed {starbot_guess}.\n"
+                                              f"The dice rolled {dice}")
+        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar)
+        await ctx.reply(embed=embed)
 
 
 async def setup(bot):
