4fb58f555bd387bd13697112b6499ebe3e1b0499
czmatejt9 <105561491+czmatejt9@users.noreply.github.com>
2022-10-27 12:01:28 +0200

    added time formatting

--- bot.py
+++ bot.py
@@ -127,6 +127,7 @@ class StarCityBot(commands.Bot):
         elif isinstance(error, commands.DisabledCommand):
             await ctx.send('Sorry. This command is disabled and cannot be used.')
         elif isinstance(error, commands.CommandOnCooldown):
+            await ctx.send(f"This command is on cooldown. Try again in {error.retry_after:.2f}s")
             time_left = formatters.format_seconds(round(error.retry_after))
             await ctx.send(f"Please wait {time_left} before using this command again")
         elif isinstance(error, (commands.ArgumentParsingError, commands.MissingRequiredArgument)):
