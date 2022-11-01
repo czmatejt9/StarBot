a91d37c50063a050ccf1e31b776565161b0253d3
czmatejt9 <105561491+czmatejt9@users.noreply.github.com>
2022-11-01 14:06:38 +0100

    debugging

--- cogs/reaction_roles.py
+++ cogs/reaction_roles.py
@@ -8,6 +8,9 @@ class ReactionRoles(commands.Cog):
     def __init__(self, bot):
         self.bot: StarCityBot = bot
 
+    def cog_check(self, ctx: commands.Context) -> bool:
+        return ctx.author.guild_permissions.administrator
+
     @commands.hybrid_command(name="reactionrole", aliases=["rr"])
     async def reaction_roles(self, ctx: commands.Context, message_id: int, emoji: str, role: discord.Role):
         """Adds a reaction role to a message. Must have admin permissions."""
@@ -20,9 +23,6 @@ class ReactionRoles(commands.Cog):
           await cursor.execute("INSERT INTO reaction_roles (message_id, emoji, role_id) VALUES (?, ?, ?)", (message_id, emoji, role.id))
           await self.bot.db.commit()
         await ctx.send("Successfully added reaction role for this server")
-
-    def cog_check(self, ctx: commands.Context) -> bool:
-        return ctx.author.guild_permissions.administrator
   
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
@@ -45,6 +45,8 @@ class ReactionRoles(commands.Cog):
             guild: discord.Guild = self.bot.get_guild(payload.guild_id)
             role: discord.Role = guild.get_role(result[0])
             await payload.member.remove_roles(role)
+
+            await self.bot.log_to_channel(f"Removed role: {role.id}")
 
 
 async def setup(bot: commands.Bot):
