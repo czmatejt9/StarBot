18ca3972e196328947fa9ac45769c570da8a0166
czmatejt9 <105561491+czmatejt9@users.noreply.github.com>
2022-11-01 13:54:36 +0100

    added removing role on removing reaction

--- cogs/reaction_roles.py
+++ cogs/reaction_roles.py
@@ -35,5 +35,19 @@ class ReactionRoles(commands.Cog):
       role: discord.Role = guild.get_role(result[0])
       await payload.member.add_roles(role)
 
+  @commands.Cog.listener()
+  async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+    async with self.bot.db.cursor() as cursor:
+      cursor: aiosqlite.Cursor
+      await cursor.execute("SELECT role_id FROM reaction_roles WHERE message_id = ? AND emoji = ?", (payload.message_id, payload.emoji.name))
+      result = await cursor.fetchone()
+    if result is not None:
+      guild: discord.Guild = self.bot.get_guild(payload.guild_id)
+      role: discord.Role = guild.get_role(result[0])
+      if role in payload.member.roles:
+        await payload.member.remove_roles(role)
+
+
+
 async def setup(bot: commands.Bot):
   await bot.add_cog(ReactionRoles(bot))
