51a43ee37c322ba0fcc0aad62434054f748e0220
czmatejt9 <105561491+czmatejt9@users.noreply.github.com>
2022-11-03 15:18:53 +0100

    fixing mistakes

--- cogs/minigames/__init__.py
+++ cogs/minigames/__init__.py
@@ -66,8 +66,7 @@ class Minigames(commands.Cog):
     async def hangman(self, ctx: commands.Context):
         """Starts a game of hangman"""
         hangman_game = hangman.Hangman(ctx.author)
-        string = ["_"] * len(hangman_game.word)
-        embed = discord.Embed(title="Hangman", description=(f"Word: {' '.join(string)}" + f"\nLives left: {hangman_game.lives}"),
+        embed = discord.Embed(title="Hangman", description=(f"Word: {'- '*len(hangman_game.word)}" + f"\nLives left: {hangman_game.lives}"),
                               color=0x00FF00)
         embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar)
         embed.set_footer(text="Click on the buttons to guess the word")
