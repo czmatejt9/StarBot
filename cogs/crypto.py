bade2e8b026a92384045fb03e9f69af312a9916c
czmatejt9 <105561491+czmatejt9@users.noreply.github.com>
2022-11-19 16:28:25 +0100

    changed ftx to coinbase

--- cogs/crypto.py
+++ cogs/crypto.py
@@ -28,7 +28,7 @@ def get_latest_bar(alpaca: REST, symbol):
     """
     Get the latest bar of stock data from Alpaca API.
     """
-    bar = alpaca.get_latest_crypto_bar(symbol, "FTXU")
+    bar = alpaca.get_latest_crypto_bar(symbol, "CBSE")
     return bar.t, bar.c
 
 
